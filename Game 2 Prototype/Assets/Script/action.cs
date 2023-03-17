4dfbb2312e36b5789652fa995ffc2a6bf7710ee5
Fin <raehanabdillah6@gmail.com>
2023-03-17 16:43:50 +0700

    Game 2
    shadow to real item

--- Game 2 Prototype/Assets/Script/action.cs
+++ Game 2 Prototype/Assets/Script/action.cs
@@ -6,11 +6,25 @@ using UnityEngine.UI;
 public class action : MonoBehaviour
 {
     public GameObject Button2_Canvas;
+    public GameObject Button3_Canvas;
     public GameObject Interact;
+    public GameObject Quiz_Canvas;
 
     public void aksi()
     {
         Interact.SetActive(true);
+        Time.timeScale = 1f;
+    }
+
+    public void aksi_2()
+    {
+        Quiz_Canvas.SetActive(true);
+        Time.timeScale = 1f;
+    }
+
+    public void close_aksi_2()
+    {
+        Quiz_Canvas.SetActive(false);
         Time.timeScale = 1f;
     }
 
@@ -27,6 +41,11 @@ public class action : MonoBehaviour
             Button2_Canvas.SetActive(true);
             Time.timeScale = 1f;
         }
+        if(col.tag == "Black")
+        {
+            Button3_Canvas.SetActive(true);
+            Time.timeScale = 1f;
+        }
     }
 
     void OnTriggerExit2D(Collider2D col)
@@ -34,6 +53,11 @@ public class action : MonoBehaviour
         if(col.tag == "Item")
         { 
             Button2_Canvas.SetActive(false);
+            Time.timeScale = 1f;
+        }
+        if(col.tag == "Black")
+        {
+            Button3_Canvas.SetActive(false);
             Time.timeScale = 1f;
         }
     }
