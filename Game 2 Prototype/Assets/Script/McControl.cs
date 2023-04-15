31470e9f8f7f0f459ea0f9bdb50f74bc93a813b4
Fabiano Abbey <abbeyfabiano@gmail.com>
2023-04-15 18:36:25 +0700

    Added Animation and Fixing Movements
    Animation and Movements for Game 2

--- Game 2 Prototype/Assets/Script/McControl.cs
+++ Game 2 Prototype/Assets/Script/McControl.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class McControl : MonoBehaviour
 {
@@ -9,11 +8,13 @@ public class McControl : MonoBehaviour
     private SpriteRenderer spi;
     private int MoveSpeed;
     private bool MoveRight, MoveLeft, MoveUp, MoveDown;
+    Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         spi = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
         MoveSpeed = 2;
         MoveRight = false;
         MoveLeft = false;
@@ -47,30 +48,80 @@ public class McControl : MonoBehaviour
         MoveLeft = false;
         MoveUp = false;
         MoveDown = false;
+        rb.velocity = Vector3.zero;
+        anim.SetBool("Right", false);
+        anim.SetBool("Left", false);
+        anim.SetBool("Up", false);
+        anim.SetBool("Down", false);
+    }
+    
+    void AnimationState()
+    {
+        if (MoveRight)
+        {
+            anim.SetBool("Right", true);
+            anim.SetBool("Left", false);
+            anim.SetBool("Up", false);
+            anim.SetBool("Down", false);
+            spi.flipX = false;
+        }
+        else if (MoveLeft)
+        {
+            anim.SetBool("Right", false);
+            anim.SetBool("Left", true);
+            anim.SetBool("Up", false);
+            anim.SetBool("Down", false);
+            spi.flipX = false;
+        }
+        else if (MoveUp)
+        {
+            anim.SetBool("Right", false);
+            anim.SetBool("Left", false);
+            anim.SetBool("Up", true);
+            anim.SetBool("Down", false);
+        }
+        else if (MoveDown)
+        {
+            anim.SetBool("Right", false);
+            anim.SetBool("Left", false);
+            anim.SetBool("Up", false);
+            anim.SetBool("Down", true);
+        }
+        else
+        {
+            anim.SetBool("Right", false);
+            anim.SetBool("Left", false);
+            anim.SetBool("Up", false);
+            anim.SetBool("Down", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (MoveRight)
+        {
+            rb.velocity = new Vector3(MoveSpeed, rb.velocity.y, 0f);
+        }
+        else if (MoveLeft)
+        {
+            rb.velocity = new Vector3(-MoveSpeed, rb.velocity.y, 0f);
+        }
+        else if (MoveUp)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, MoveSpeed, 0f);
+        }
+        else if (MoveDown)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, -MoveSpeed, 0f);
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
     }
 
     void Update()
     {
-        if(MoveRight)
-        {
-            rb.velocity = new Vector3(MoveSpeed,0,0);
-            spi.flipX = true;
-        }
-
-        if(MoveLeft)
-        {
-            rb.velocity = new Vector3(-MoveSpeed,0,0);
-            spi.flipX = false;
-        }
-
-        if(MoveUp)
-        {
-            rb.velocity = new Vector3(0,MoveSpeed,0);
-        }
-
-        if(MoveDown)
-        {
-            rb.velocity = new Vector3(0,-MoveSpeed,0);
-        }
+        AnimationState();
     }
 }
